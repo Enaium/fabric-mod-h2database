048b3f0df5a5dbdbab6f6c9d95773fb1fabd028c
Enaium <wyy-666@hotmail.com>
2025-07-20 17:29:01 +0800

    maven publish

--- README.md
+++ README.md
@@ -1,7 +1,54 @@
 # Fabric Database H2
 
+[![Version](https://img.shields.io/github/v/tag/Enaium/fabric-mod-h2database?label=version&style=flat-square&logo=github)](https://github.com/Enaium/fabric-mod-h2database/releases)
+[![CurseForge Downloads](https://img.shields.io/curseforge/dt/1278174?style=flat-square&logo=curseforge)](https://www.curseforge.com/minecraft/mc-mods/fabric-database-h2)
+[![Modrinth Downloads](https://img.shields.io/modrinth/dt/AWt0yK0m?style=flat-square&logo=modrinth)](https://modrinth.com/mod/fabric-database-h2)
+
 This is a mod that enables the use of the H2 database for Fabric mods.
 
 Note: This does not add content! This is a dependency that other mods use for compatibility.
 
 This mod works independently of the Minecraft version used, as it only requires Fabric Loader to be available.
+
+## Installation Guide
+
+### For Development (Mod Developers)
+
+To use Fabric Database H2 as a dependency in your Fabric mod:
+
+1. Add the following to your `build.gradle` or `build.gradle.kts`:
+
+   **Groovy (build.gradle):**
+   ```groovy
+   repositories {
+       mavenCentral()
+   }
+   dependencies {
+       modImplementation 'cn.enaium:fabric-database-h2:<version>'
+   }
+   ```
+   **Kotlin DSL (build.gradle.kts):**
+   ```kotlin
+   repositories {
+       mavenCentral()
+   }
+   dependencies {
+       modImplementation("cn.enaium:fabric-database-h2:<version>")
+   }
+   ```
+   Replace `<version>` with the latest version from the [releases page](https://github.com/Enaium/fabric-mod-h2database/releases).
+
+2. Refresh your dependencies and import as usual.
+
+### For Players
+
+1. Download the latest release from one of the following:
+   - [CurseForge](https://www.curseforge.com/minecraft/mc-mods/fabric-database-h2)
+   - [Modrinth](https://modrinth.com/mod/fabric-database-h2)
+   - [GitHub Releases](https://github.com/Enaium/fabric-mod-h2database/releases)
+
+2. Place the downloaded `.jar` file into your Minecraft `mods` folder.
+
+3. Make sure you have [Fabric Loader](https://fabricmc.net/use/) installed.
+
+4. Launch Minecraft as usual.
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,30 @@
 plugins {
     alias(libs.plugins.loom)
+    alias(libs.plugins.publish)
+    alias(libs.plugins.maven)
 }
 
 group = "cn.enaium"
-version = "1.0.0+h2.${libs.versions.h2.get()}"
+version = "1.0.1+h2.${libs.versions.h2.get()}"
 
 repositories {
     mavenCentral()
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+val includeAndExpose: Configuration by configurations.creating
+
+configurations {
+    include {
+        extendsFrom(includeAndExpose)
+    }
+    modApi {
+        extendsFrom(includeAndExpose)
+    }
 }
 
 dependencies {
@@ -17,12 +35,80 @@ dependencies {
         }
     }
     modImplementation(libs.fabric.loader)
-    include(libs.h2)
+    includeAndExpose(libs.h2)
 }
 
 tasks.processResources {
     inputs.property("version", version)
     filesMatching("fabric.mod.json") {
         expand("version" to version)
+    }
+}
+
+publishMods {
+    file = tasks.remapJar.get().archiveFile.get()
+    type = STABLE
+    displayName = "Fabric Database H2 ${project.version}"
+    changelog = ""
+    modLoaders.add("fabric")
+
+    curseforge {
+        projectId = "1278174"
+        accessToken = providers.gradleProperty("curseforge.token")
+        minecraftVersionRange {
+            start = "1.0"
+            end = "latest"
+        }
+    }
+
+    modrinth {
+        projectId = "AWt0yK0m"
+        accessToken = providers.gradleProperty("modrinth.token")
+        minecraftVersionRange {
+            start = "1.0"
+            end = "latest"
+        }
+    }
+
+    github {
+        repository = "Enaium/fabric-mod-h2database"
+        accessToken = providers.gradleProperty("github.token")
+        commitish = "master"
+    }
+}
+
+mavenPublishing {
+
+    publishToMavenCentral(automaticRelease = true)
+
+    signAllPublications()
+
+    coordinates(
+        groupId = project.group.toString(),
+        artifactId = project.name,
+        version = project.version.toString()
+    )
+
+    pom {
+        name = "Fabric H2 Database"
+        description = "Minecraft Database Library"
+        url = "https://github.com/Enaium/fabric-mod-h2database"
+        licenses {
+            license {
+                name = "Mozilla Public License Version 2.0"
+                url = "https://www.mozilla.org/en-US/MPL/2.0/"
+            }
+        }
+        developers {
+            developer {
+                name = "Enaium"
+                url = "https://github.com/Enaium"
+            }
+        }
+        scm {
+            connection.set("scm:git:git://github.com/Enaium/fabric-mod-h2database.git")
+            developerConnection.set("scm:git:ssh://github.com/Enaium/fabric-mod-h2database.git")
+            url.set("https://github.com/Enaium/fabric-mod-h2database")
+        }
     }
 }
